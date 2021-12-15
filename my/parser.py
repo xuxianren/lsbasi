4704b1dfa8d05e8de17e3eecd7b75f48fff80f08
xudalei <602159996@qq.com>
2021-12-15 21:33:20 +0800

    dev: parser

--- my/parser.py
+++ my/parser.py
@@ -1,5 +1,29 @@
 from .token import *
 
+whitespace = ' \t\n\r\v\f'
+ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
+ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ascii_letters = ascii_lowercase + ascii_uppercase
+digits = '0123456789'
+
+punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+printable = digits + ascii_letters + punctuation + whitespace
+
+id_start = ascii_letters + "_"
+id_content = ascii_letters + digits + "_"
+operator = [
+	"+",
+	"-",
+	"*",
+	"/",
+	"<",
+	"<=",
+	">",
+	">=",
+	"!=",
+	"==",
+]
+
 KEY_WORLD = {
     "IF"
     
@@ -14,20 +38,26 @@ class Parser:
         self._p2 = 0
 
     def read_id(self):
+        pass
 
+    def get_identifier(self, ):
+        pass
+    
+    def get_number(self, ):
+        pass
+    
+    def get_oprator(self):
+        pass
 
     def get_next_token(self):
         if self._p1 >= len(self.text):
             return 
-        raw = ""
         char = self.text[self._p1]
-        if char == "=":
-            return 
-        elif char in "a-zA-Z_":
-            return 
-        elif char in "0-9":
-            return 
-        elif char in "+-*/":
-            return 
-        elif char in " \n":
-            return 
+        if char in id_start:
+            return self.get_identifier()
+        elif char in digits:
+            return self.get_number()
+        elif char in operator:
+            return self.get_oprator()
+        elif char in whitespace:
+            return self.get_next_token()
